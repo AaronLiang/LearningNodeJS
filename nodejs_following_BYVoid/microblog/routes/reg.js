38909ef90f008f33aa06a57b126e8fe8618e01d6
RockyNiu <lei.niu.ny@gmail.com>
2014-05-20 09:37:14 -0400

    update redirect

---
+++ nodejs_following_BYVoid/microblog/routes/reg.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var reg = express.Router();
+
+/* GET registration page. */
+var crypto = require('crypto');
+var User = require('../models/user.js');
+
+reg.get('/', function(req, res) {
+	res.render('reg', {
+		title : '用户注册'
+//		user : req.session.user,
+//		success : req.flash('success').toString(),
+//		error : req.flash('error').toString()
+	});
+});
+
+/* POST registration page. */
+reg.post('/', function(req, res) {
+	// 检测用户两次输入的口令是否一致
+	if (req.body['password-repeat'] != req.body['password']) {
+		req.flash('error', '两次输入的口令不一致');
+		return res.redirect('/reg');
+	}
+	// 生成口令的散列值
+	var md5 = crypto.createHash('md5');
+	var password = md5.update(req.body.password).digest('base64');
+
+	var newUser = new User({
+		name : req.body.username,
+		password : password,
+	});
+
+	// 检查用户是否已经存在
+	User.get(newUser.name, function(err, user) {
+		if (user)
+			err = '用户已存在。';
+		if (err) {
+			req.flash('error', err);
+			return res.redirect('/reg');
+		}
+		// 如果不存在则新增用户
+		newUser.save(function(err) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/reg');
+			}
+			req.session.user = newUser;
+			req.flash('success', '祝贺您注册成功！');
+			return res.redirect('/');
+		});
+	});
+});
+
+console.log('reg.js');
+module.exports = reg;
